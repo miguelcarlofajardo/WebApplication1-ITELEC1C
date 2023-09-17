eaac076ab9779a8b9cc7629a2568805bee4c04ec
Miguel <miguelcarlo.fajardo.cics@ust.edu.ph>
2023-09-17 18:24:24 +0800

    Creation of Machine Activity 1

---
+++ WebApplication1/Controllers/MA1Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class MA1Controller : Controller
+    {
+        List<MA1> MA1List = new List<MA1>
+            {
+                new MA1()
+                {
+                    FirstName = "Juan",LastName = "Dela Cruz",IsTenured = true
+                }
+            };
+        public IActionResult Index()
+        {
+
+            return View(MA1List);
+        }
+    }
+}
---
+++ WebApplication1/Models/MA1.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+
+    public class MA1
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsTenured { get; set; }
+    }
+}
+
